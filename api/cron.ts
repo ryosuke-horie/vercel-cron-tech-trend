28be6eb6cdcd940b2a8b1bba672c05c0cfcadbba
ryosuke-horie <ryosuke03140@gmial.com>
2023-05-07 08:13:22 +0900

    サーバレス関数の形にしてまとめる

---
+++ api/cron.ts
@@ -0,0 +1,18 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import getQiitaTrend from '../lib/qiita';
+import getZennTrend from '../lib/zenn';
+import sendMail from '../lib/send-main';
+
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse,
+) {
+    // メール送信用の文章を取得
+    const QiitaText = getQiitaTrend()
+    const ZennText  = getZennTrend()
+    
+    const mailBodyText = await QiitaText + await ZennText
+
+    // メール送信処理
+    return sendMail(mailBodyText)
+}
